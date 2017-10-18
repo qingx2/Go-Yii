44da9812579d07981156450867afbbac1d66e641
Zachary <zachary.mmb@gmail.com>
2017-10-18 23:09:51 +0800

    enablePrettyUrl全局

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,12 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName'  => false,
+            'rules'           => [
+            ],
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
